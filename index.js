10dbd8197401efb4271dcfa8ac0165b9aab1ad5c
Reuben T Allik <allik@bc.edu>
2021-01-24 15:28:36 -0500

    1.24

--- index.js
+++ index.js
@@ -1,16 +1,42 @@
-class ToDo {
-    constructor(task, datetime) {
-        this.task = task;
-        this.datetime = datetime;
-    }
+// class ToDo {
+//     constructor(task) {
+//         this.task = task;
+//     }
+// }
 
-    getTimeElapsed() {
-        const rightnow = new Date().getTime() - this.datetime;
-        return `The task: ${this.task}, is ${this.datetime} old.`;
-    }
+function formatTaskForList(taskText) {
+    // const newTask = new ToDo(taskText)
+    console.log(taskText);
+    const todoelement = document.createElement("li");
+    todoelement.classList.add("taskListElement");
+    const todoelement_inner = 
+        `<button class="taskComplete"></button>
+        ${taskText}
+        <button class="deleteTask"></button>`;
+    todoelement.innerHTML = todoelement_inner;
+    console.log(typeof todoelement);
+    taskList.appendChild(todoelement);
+    
+
 }
 
-function addTask() {
-    const newTask = document.getElementById('task');
+
+let taskTextInput = document.getElementById("taskTextInput");
+let addTask = document.getElementById("addTask");
+let taskList = document.getElementById("taskList");
+
+let taskComplete = document.getElementById("taskComplete");
+let deleteTask = document.getElementById("deleteTask");
+
+
+addTask.addEventListener("click", function(){
+    let newTask = taskTextInput.value;
     console.log(newTask);
-}
+    let formattedTask = formatTaskForList(newTask);
+    console.log(typeof formattedTask);
+})
+
+// taskComplete.addEventListener("click", function(){
+//     console.log('task complete clic')
+//     todoelement.classList.add("taskcomplete");
+// })
